beb43b69413bcb5845fc9d814540cdfdaa14daef
Oleg Cherednik <oleg.cherednik@gmail.com>
2024-03-09 08:09:40 +0300

    json-api

--- src/main/java/ru/olegcherednik/json/api/enumid/EnumId.java
+++ src/main/java/ru/olegcherednik/json/api/enumid/EnumId.java
@@ -42,16 +42,32 @@ public interface EnumId {
     }
 
     static <T extends Enum<?> & EnumId> T parseName(Class<T> cls, String name) {
-        T res = parseName(cls.getEnumConstants(), name, null);
-
-        if (res != null)
-            return res;
-
-        throw new EnumConstantNotPresentException(cls, name);
+        return Optional.ofNullable(parseName(cls.getEnumConstants(), name, null))
+                       .orElseThrow(() -> new EnumConstantNotPresentException(cls, name));
     }
 
     static <T extends Enum<?> & EnumId> T parseName(Class<T> cls, String name, T def) {
         return parseName(cls.getEnumConstants(), name, def);
+    }
+
+    static <T extends Enum<?> & EnumId> T parseIdOrName(Class<T> cls, String idOrName) {
+        return Optional.ofNullable(parseIdOrName(cls, idOrName, null))
+                       .orElseThrow(() -> new EnumConstantNotPresentException(cls, idOrName));
+    }
+
+    static <T extends Enum<?> & EnumId> T parseIdOrName(Class<T> cls, String idOrName, T def) {
+        T[] values = cls.getEnumConstants();
+        return Optional.ofNullable(parseId(values, idOrName, null))
+                       .orElseGet(() -> parseName(values, idOrName, def));
+    }
+
+    static <T extends Enum<?> & EnumId> T parseId(Class<T> cls, String id) {
+        return Optional.ofNullable(parseId(cls.getEnumConstants(), id, null))
+                       .orElseThrow(() -> new EnumConstantNotPresentException(cls, id));
+    }
+
+    static <T extends Enum<?> & EnumId> T parseId(Class<T> cls, String id, T def) {
+        return parseId(cls.getEnumConstants(), id, def);
     }
 
     static <T extends EnumId> T parseName(T[] values, String name, T def) {
@@ -60,25 +76,6 @@ public interface EnumId {
                 return value;
 
         return def;
-    }
-
-    static <T extends Enum<?> & EnumId> T parseIdOrName(Class<T> cls, String idOrName) {
-        T res = parseId(cls.getEnumConstants(), idOrName, null);
-        return Optional.ofNullable(res).orElseGet(() -> parseName(cls, idOrName));
-    }
-
-    static <T extends Enum<?> & EnumId> T parseIdOrName(Class<T> cls, String idOrName, T def) {
-        T res = parseId(cls.getEnumConstants(), idOrName, null);
-        return Optional.ofNullable(res).orElseGet(() -> parseName(cls, idOrName, def));
-    }
-
-    static <T extends Enum<?> & EnumId> T parseId(Class<T> cls, String id) {
-        T res = parseId(cls.getEnumConstants(), id, null);
-        return Optional.ofNullable(res).orElseThrow(() -> new EnumConstantNotPresentException(cls, id));
-    }
-
-    static <T extends Enum<?> & EnumId> T parseId(Class<T> cls, String id, T def) {
-        return parseId(cls.getEnumConstants(), id, def);
     }
 
     static <T extends EnumId> T parseId(T[] values, String id, T def) {
